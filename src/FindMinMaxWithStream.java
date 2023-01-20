7ef41d93a8e17c3a47d8de2e4858e2ab771b50e1
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2023-01-20 21:19:07 +0300

    MinMax

--- src/FindMinMaxWithStream.java
+++ src/FindMinMaxWithStream.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class FindMinMaxWithStream {
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class FindMinMaxWithStream {
+    public static void findMinMax(Stream<Integer> stream, Comparator<Integer> order,
+                                  BiConsumer<Integer, Integer> minMaxConsumer) {
+        List<Integer> minMaxList = stream.toList();
+        Integer min = minMaxList.stream().min(order).orElse(null);
+        Integer max = minMaxList.stream().max(order).orElse(null);
+        minMaxConsumer.accept(min, max);
+    }
+
+    public static void main(String[] args) {
+        BiConsumer<Integer, Integer> biConsumer =
+                ((integer, integer2) -> System.out.println(" Min " + integer + "\n Max " + integer2));
+        findMinMax(IntStream.rangeClosed(1, 100).boxed(), Comparator.naturalOrder(), biConsumer);
+    }
 }
